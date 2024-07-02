311175a65cb413bb291d37f3fb30f97b2bedee84
njugunabriian <143797807+ifconfigbrian@users.noreply.github.com>
2024-07-02 12:15:17 +0300

    updated classes

--- Lessons.php
+++ Lessons.php
@@ -6,8 +6,30 @@
     <title>Classes</title>
 </head>
 <body>
-
+    <!-- the form is here for visiblity purposes,this is actually lesson 5 -->
+<!-- lesson5-working with forms -->
+<form action="Lessons.php" method="post"> 
+    <!-- when you use get the information can be modified inthe url,use post when you don't want the input to be modified in the url -->
+    Name: <input type="text" name = "name"><br>
+    <input type="submit">
+</form>
+<br>
+<!-- lesson 6-more practice with forms-mad libs game -->
+ <form action="Lessons.php">
+    Name: <input type="text" name = "flower"><br>
+    Noun: <input type="text" name = "noun"><br>
+    Color: <input type="text" name = "color"><br>
+    <input type="submit">
+ </form>
 <?php 
+$Name = $_POST["flower"];
+$Noun = $_POST["noun"];
+$Color = $_POST["color"];
+echo "Roses are$Color violets are$Color i love$Name";
+//now lets get the name submitted from our form
+ echo "Your name is: ". $_POST["name"];//use the . operator to concatenate
+//  when you use get the information can be modified inthe url,use post when you don't want the input to be modified in the url
+
 //php inside html-lesson1(beginning)
 
 echo "<h1>Hello Brian</h1>";
@@ -50,7 +72,30 @@ echo $number / 20;//this will just do the math you want it to do
 echo $number * 20;//this will just do the math you want it to do
 echo $number ++;//this will just do the math you want it to do
 echo $number --;//this will just do the math you want it to do
+
+//lesson7-arrays
+$friends = array("Brian","Michael","Ryan","Creed");
+echo $friends[0];//arrays indexing starts at 0 so this will print out Brian
+echo $friends[0] = "David";//this will change th evalue of the first index to David
+echo $friends[0] ;//print out the first index to confirm
+
+//lesson8-associative arrays-this are arrays that have a key and value pair,each key must be unique
+$grades = array("Brian"=>"A","Michael"=>"B","Ryan"=>"C","Creed"=>"E");
+echo $grades["Brian"];//this will print out Brians grade
+echo $grades["Brian"] = "B+";//this change his grade
+echo $grades["Brian"];//this display his new grade his grade
+echo count($grades);//this counts how many items are in the array
+
+//less0n9-functions and parameters
+function sayHi($name){//name is a parameter
+    echo "Ohoey $name";
+}
+//you must call a function and pass a value as per the parameters
+sayHi("Dwight");
+sayHi("Creed");
+sayHi("Michael");
 ?>
+
     
 </body>
 </html>
